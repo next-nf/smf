0ad41da074db6edaa8d801576aae245f2d32b149
Nathan Foster <next-nf@proton.me>
2026-04-07 21:20:22 +0200

    remove raft lifecycle from ergw_cluster

--- apps/ergw_cluster/src/ergw_cluster.erl
+++ apps/ergw_cluster/src/ergw_cluster.erl
@@ -14,7 +14,6 @@
 %% API
 -export([start_link/0, start/1,
 	 validate_options/1,
-	 get_ra_node_id/0,
 	 wait_till_ready/0,
 	 is_ready/0]).
 
@@ -65,20 +64,12 @@ is_running() ->
 %%%===================================================================
 
 -define(ClusterDefaults, [{enabled, false},
-			  {initial_timeout, 60 * 1000},
-			  {release_cursor_every, 0},
 			  {seed_nodes, {erlang, nodes, [known]}}]).
 
 validate_options(Values) ->
     ergw_core_config:validate_options(fun validate_option/2, Values, ?ClusterDefaults).
 
 validate_option(enabled, Value) when is_boolean(Value) ->
-    Value;
-validate_option(initial_timeout, Value)
-  when is_integer(Value), Value > 0 ->
-    Value;
-validate_option(release_cursor_every, Value)
-  when is_integer(Value) ->
     Value;
 validate_option(seed_nodes, {M, F, A} = Value)
   when is_atom(M), is_atom(F), is_list(A) ->
@@ -172,11 +163,11 @@ startup() ->
 start_cluster(Config) ->
     do([error_m ||
 	   maybe_start_k8s_dist(Config),
-	   start_raft()
+	   ergw_global:create(),
+	   gtp_config:init()
        ]).
 
 stop_cluster() ->
-    leave_ra_cluster(10),
     ok.
 
 maybe_start_k8s_dist(#{enabled := true}) ->
@@ -189,96 +180,3 @@ maybe_start_k8s_dist(#{enabled := true}) ->
     end;
 maybe_start_k8s_dist(_) ->
     ok.
-
-%%%===================================================================
-%%% RAFT cluster
-%%%===================================================================
-
-get_ra_node_id() ->
-    application:get_env(ergw_cluster, ra_node_id, '$ra-ergw-cluster').
-
-start_ra_cluster(cluster, ClusterId, Machine, ServerId) ->
-    ra:start_cluster(ClusterId, Machine, [ServerId]);
-start_ra_cluster(server, ClusterId, Machine, ServerId) ->
-    ra:start_server(ClusterId, ServerId, Machine, []).
-
-start_ra_cluster(Mode, ServerId) ->
-    ServerId = {get_ra_node_id(), node()},
-    ClusterId = ergw_core,
-    Config = #{},
-    Machine = {module, ergw_global, Config},
-
-    case start_ra_cluster(Mode, ClusterId, Machine, ServerId) of
-	{ok, _, _} -> ok;
-	Other      -> Other
-    end.
-
-join_ra_cluster(Leader) ->
-    ServerId = {get_ra_node_id(), node()},
-    do([error_m ||
-	   start_ra_cluster(server, ServerId),
-	   add_ra_member(Leader, ServerId),
-	   gtp_config:sync()
-       ]).
-
-add_ra_member(Leader, ServerId) ->
-    case ra:add_member(Leader, ServerId) of
-	{error, cluster_change_not_permitted} ->
-	    timer:sleep(100),
-	    add_ra_member(Leader, ServerId);
-	{ok, _, _} ->
-	    ok;
-	Other      -> Other
-    end.
-
-leave_ra_cluster(0) ->
-    {error, timeout};
-leave_ra_cluster(Cnt) ->
-    NodeId = get_ra_node_id(),
-    case whereis(NodeId) of
-	Pid when is_pid(Pid) ->
-	    case (catch ra:leave_and_delete_server({NodeId, node()})) of
-		ok ->
-		    ok;
-		{error, Error} when Error == noproc; Error == cluster_change_not_permitted ->
-		    timer:sleep(100),
-		    leave_ra_cluster(Cnt - 1);
-		{'EXIT',_} ->
-		    timer:sleep(100),
-		    leave_ra_cluster(Cnt - 1);
-		Other ->
-		    ?LOG(error, "ra leave failed with ~p", [Other]),
-		    Other
-	    end;
-	_ ->
-	    ok
-    end.
-
-query_ra_cluster() ->
-    ServerIds =
-	[X || {ok, {NId, _} = X} <- erpc:multicall(nodes(), fun() -> {get_ra_node_id(), node()} end), is_atom(NId)],
-    query_ra_cluster(ServerIds).
-
-query_ra_cluster([]) ->
-    standalone;
-query_ra_cluster([H|T]) ->
-    case ra:members(H) of
-	{ok, _Members, Leader} ->
-	    {ok, Leader};
-	_ ->
-	    query_ra_cluster(T)
-    end.
-
-start_raft() ->
-    global:trans({'$ergw', self()}, fun start_raft_sync/0).
-
-start_raft_sync() ->
-    case query_ra_cluster() of
-	{ok, Leader} ->
-	    join_ra_cluster(Leader);
-	_ ->
-	    do([error_m ||
-		   start_ra_cluster(cluster, {get_ra_node_id(), node()}),
-		   gtp_config:init()
-	       ])
-    end.
